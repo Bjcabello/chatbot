7099195fb01f190ed3391e0b7b5ffb1f77d2856d
Bryan Cabello <bjcabello55@gmail.com>
2025-07-22 11:47:55 -0500

    modificanto estructura

--- src/utils/chroma.py
+++ src/utils/chroma.py
@@ -8,6 +8,6 @@ collection = client.get_or_create_collection(CHROMA_COLLECTION)
 def indexar_documento(nombre, contenido):
     collection.add(documents=[contenido], ids=[nombre])
 
-def buscar_fragmentos_relevantes(pregunta: str) -> str:
+def  buscar_fragmentos_relevantes(pregunta: str) -> str:
     resultados = collection.query(query_texts=[pregunta], n_results=2)
     return "\n".join(resultados["documents"][0])
